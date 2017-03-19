a1a77af4e6e28ad6034b7d7c440df57b18993273
RaphaelParment <raphparment@gmail.com>
2017-03-19 22:31:07 +0100

    Config files.

---
+++ Config.cpp
@@ -0,0 +1,10 @@
+/*
+ * Config.cpp
+ *
+ *  Created on: Mar 19, 2017
+ *      Author: raphish
+ */
+
+#include "Config.h"
+
+vector<char> noisyChars = {',', '(', ')'};
---
+++ Config.h
@@ -0,0 +1,19 @@
+/*
+ * Config.h
+ *
+ *  Created on: Mar 19, 2017
+ *      Author: raphish
+ */
+
+#include <vector>
+
+using namespace std;
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+
+extern vector<char> noisyChars;
+
+
+#endif /* CONFIG_H_ */
